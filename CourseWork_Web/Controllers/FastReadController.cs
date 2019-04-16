b94a425a1cee509763bf8b29a0bcf4bdd7b6e061
abai <abai2010@bk.ru>
2019-04-17 00:29:22 +0600

    добавил FastRead (миксер букв в словах)

---
+++ CourseWork_Web/Controllers/FastReadController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseWork_Web.Controllers
+{
+    public class FastReadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //первая, вторая и посл буква остаются
+        //через каждое третье слово
+        [HttpPost]
+        public IActionResult Process(string inputText)
+        {
+            List<string> words = inputText.Split(" ,.!?-".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    words[i] = ShuffleWord(words[i]);
+                }
+
+            }
+
+            return Json(string.Join(" ", words));
+        }
+
+        public string ShuffleWord(string word)
+        {
+            char[] array = word.ToCharArray();
+            Random rng = new Random();
+            int n = array.Length - 2;
+            while (n > 1)
+            {
+
+                int k = rng.Next(2, n+1);
+                var value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+                n--;
+            }
+            return new string(array);
+        }
+    }
+}
